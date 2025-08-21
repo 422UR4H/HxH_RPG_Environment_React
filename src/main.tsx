5663f2cfa5e9ef9db040486c702c64ab4a9376fe
422UR4H <tiagosindra@id.uff.br>
2025-08-20 22:11:21 -0300

    chore: set react query client

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,30 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import ResetStyle from "./styles/ResetStyle";
 import { UserProvider } from "./contexts/UserContext";
 import { TokenProvider } from "./contexts/TokenContext";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      // staleTime: 5 * 60 * 1000, // 5 minutos
+      // cacheTime: 10 * 60 * 1000, // 10 minutos
+    },
+  },
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ResetStyle />
-    <UserProvider>
-      <TokenProvider>
-        <App />
-      </TokenProvider>
-    </UserProvider>
+    <QueryClientProvider client={queryClient}>
+      <UserProvider>
+        <TokenProvider>
+          <App />
+        </TokenProvider>
+      </UserProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
