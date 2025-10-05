786ef8cce47d1856416ef5129fad914237fa25e6
422UR4H <tiagosindra@id.uff.br>
2025-10-05 15:17:59 -0300

    fix: allow page header to receibe background color

--- src/components/atoms/PageHeader.tsx
+++ src/components/atoms/PageHeader.tsx
@@ -4,24 +4,25 @@ import LogoButton from "./LogoButton";
 
 interface PageHeaderProps {
   to: string;
+  backgroundColor?: string;
 }
 
-export default function PageHeader({ to }: PageHeaderProps) {
+export default function PageHeader({ to, backgroundColor }: PageHeaderProps) {
   return (
-    <StyledPageHeader>
+    <StyledPageHeader $backgroundColor={backgroundColor}>
       <BackButton to={to} />
       <LogoButton />
     </StyledPageHeader>
   );
 }
 
-const StyledPageHeader = styled.div`
+const StyledPageHeader = styled.div<{ $backgroundColor?: string }>`
   display: flex;
   align-items: center;
   justify-content: center;
   flex-shrink: 0;
 
-  background-color: #252525;
+  background-color: ${({ $backgroundColor }) => $backgroundColor || "#252525"};
   width: 100%;
   height: min(102px, 15.2vw);
   padding-bottom: 0.4vw;
