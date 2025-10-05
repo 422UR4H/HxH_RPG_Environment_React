3a540c72550b75f82cc2cd96dd4e2e8d592642b2
422UR4H <tiagosindra@id.uff.br>
2025-10-05 19:33:29 -0300

    refactor: fix text align style in page title

--- src/components/ions/PageTitle.tsx
+++ src/components/ions/PageTitle.tsx
@@ -12,7 +12,7 @@ const StyledPageTitle = styled.h1`
   font-family: "Roboto", sans-serif;
   font-weight: 900;
   font-size: 46px;
-  text-align: center;
+  /* text-align: center; */
   color: white;
   margin: 0;
   font-size: min(40px, 6.8vw);
