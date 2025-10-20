ea4b430a62eba453078b4fe0ec0dd307e74cd21d
422UR4H <tiagosindra@id.uff.br>
2025-10-20 00:26:56 -0300

    feat: add styles to improve ux

--- src/components/ions/MinusButton.tsx
+++ src/components/ions/MinusButton.tsx
@@ -1,17 +1,43 @@
 import styled from "styled-components";
 import minusIcon from "../../assets/icons/minus.svg";
 
-export default function MinusButton() {
+interface MinusButtonProps {
+  onClick?: () => void;
+  isDisabled?: boolean;
+}
+
+export default function MinusButton({
+  onClick,
+  isDisabled = false,
+}: MinusButtonProps) {
   return (
-    <MinusButtonContainer>
+    <MinusButtonContainer
+      onClick={isDisabled ? undefined : onClick}
+      $disabled={isDisabled}
+    >
       <img src={minusIcon} alt="-" />
     </MinusButtonContainer>
   );
 }
 
-const MinusButtonContainer = styled.div`
+const MinusButtonContainer = styled.div<{ $disabled: boolean }>`
   width: 8cqi;
   height: 7.8cqi;
   padding: 1.7cqi 0;
-  cursor: pointer;
+
+  cursor: ${({ $disabled }) => ($disabled ? "not-allowed" : "pointer")};
+  filter: ${({ $disabled }) => ($disabled ? "grayscale(100%)" : "none")};
+
+  transition: transform 0.2s ease, filter 0.2s ease;
+
+  pointer-events: ${({ $disabled }) => ($disabled ? "none" : "auto")};
+
+  &:hover {
+    ${({ $disabled }) =>
+      !$disabled &&
+      `
+      filter: brightness(1.1);
+      transform: scale(1.3);
+    `}
+  }
 `;
--- src/components/ions/PlusButton.tsx
+++ src/components/ions/PlusButton.tsx
@@ -1,16 +1,42 @@
 import styled from "styled-components";
 import plusIcon from "../../assets/icons/plus.svg";
 
-export default function PlusButton() {
+interface PlusButtonProps {
+  onClick?: () => void;
+  isDisabled?: boolean;
+}
+
+export default function PlusButton({
+  onClick,
+  isDisabled = false,
+}: PlusButtonProps) {
   return (
-    <PlusButtonContainer>
+    <PlusButtonContainer
+      onClick={isDisabled ? undefined : onClick}
+      $disabled={isDisabled}
+    >
       <img src={plusIcon} alt="+" />
     </PlusButtonContainer>
   );
 }
 
-const PlusButtonContainer = styled.div`
+const PlusButtonContainer = styled.div<{ $disabled: boolean }>`
   width: 8cqi;
   height: 7.98cqi;
-  cursor: pointer;
+
+  cursor: ${({ $disabled }) => ($disabled ? "not-allowed" : "pointer")};
+  filter: ${({ $disabled }) => ($disabled ? "grayscale(100%)" : "none")};
+
+  transition: transform 0.2s ease, filter 0.2s ease;
+
+  pointer-events: ${({ $disabled }) => ($disabled ? "none" : "auto")};
+
+  &:hover {
+    ${({ $disabled }) =>
+      !$disabled &&
+      `
+      filter: brightness(1.1);
+      transform: scale(1.2);
+    `}
+  }
 `;
