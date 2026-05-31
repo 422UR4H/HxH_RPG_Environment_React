795ba295bb8791cd59a79aa842ebe8e8197345f1
422UR4H <tiagosindra@id.uff.br>
2026-05-31 20:40:53 -0300

    feat: add mapsService with CRUD operations

---
+++ src/services/mapsService.ts
@@ -0,0 +1,52 @@
+import { httpClient } from "./httpClient";
+import { objToCamelCase, objToSnakeCase } from "../utils/caseConverter";
+import config from "./config";
+import type { TacticalMap } from "../types/tacticalMap";
+
+export const mapsService = {
+  createMap: (
+    token: string,
+    campaignId: string,
+    data: { name: string; description?: string },
+  ): Promise<TacticalMap> =>
+    httpClient
+      .post<{ map: TacticalMap }>(
+        `/campaigns/${campaignId}/maps`,
+        objToSnakeCase(data),
+        config(token),
+      )
+      .then(({ data: res }) => objToCamelCase<TacticalMap>(res.map)),
+
+  listMaps: (token: string, campaignId: string): Promise<TacticalMap[]> =>
+    httpClient
+      .get<{ maps: TacticalMap[] }>(
+        `/campaigns/${campaignId}/maps`,
+        config(token),
+      )
+      .then(({ data: res }) =>
+        objToCamelCase<{ maps: TacticalMap[] }>(res).maps ?? [],
+      ),
+
+  getMap: (token: string, mapId: string): Promise<TacticalMap> =>
+    httpClient
+      .get<{ map: TacticalMap }>(`/maps/${mapId}`, config(token))
+      .then(({ data: res }) => objToCamelCase<TacticalMap>(res.map)),
+
+  updateMap: (
+    token: string,
+    mapId: string,
+    data: object,
+  ): Promise<TacticalMap> =>
+    httpClient
+      .put<{ map: TacticalMap }>(
+        `/maps/${mapId}`,
+        objToSnakeCase(data),
+        config(token),
+      )
+      .then(({ data: res }) => objToCamelCase<TacticalMap>(res.map)),
+
+  deleteMap: (token: string, mapId: string): Promise<void> =>
+    httpClient
+      .delete(`/maps/${mapId}`, config(token))
+      .then(() => undefined),
+};
